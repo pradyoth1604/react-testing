adbe09e918f65db56943a99f1562b0bddb171695
pradyoth1604 <pradyothreddy1604@gmail.com>
2024-06-07 18:27:13 +0530

    Added Test for form application :)

---
+++ src/components/Application/Application.tsx
@@ -0,0 +1,28 @@
+export const Application = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" placeholder="Type name....." />
+        </div>
+        <div>
+          <label htmlFor="Job-location">Job Location</label>
+          <select id="job-location">
+            <option>Select State</option>
+            <option>Hyderabad</option>
+            <option>Bangalore</option>
+            <option>Pune</option>
+          </select>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" id="terms" />I agree to the terms and
+            condition
+          </label>
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
--- src/components/Greet/Greet.test.tsx
+++ src/components/Greet/Greet.test.tsx
@@ -1,10 +1,16 @@
 import { render, screen } from "@testing-library/react"
 import { Greet } from "./Greet"
 
-test("Greet renders react",()=>{
-    render(<Greet name="pradyoth"/>)
-    const textElement = screen.getByText(/hello pradyoth/i);
-    const text = screen.getByText(/hello/i);
-    expect(textElement).toBeInTheDocument();
-    expect(text).toBeInTheDocument();
+describe("Greet",()=>{
+    test("renders Correctly",()=>{
+        render(<Greet/>);
+        const textElement = screen.getByText(/hello/i);
+        expect(textElement).toBeInTheDocument();
+    })
+    
+    test("renders name",()=>{
+        render(<Greet name="Pradyoth"/>)
+        const textElement = screen.getByText(/hello pradyoth/i);
+        expect(textElement).toBeInTheDocument();
+    })
 })
